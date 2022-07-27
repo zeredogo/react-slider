c18d0b43424f84f8ffcce9dcdb5190ed98a25685
zeredogo <zeredogo@gmail.com>
2022-07-27 13:52:05 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -19,11 +19,26 @@ const App = () => {
         {people.map((person, personIndex) =>{
          const {id, image, name, title, quote} = person;
          // more stuff comming up
+         let position = 'nextSlider';
+         if (personIndex === index) {
+          position = 'activeSlide';
+         }
          
-         return <article key={id}>
+         return (<article className={position} key={id}>
           <img src={image} alt={name} className='person-img'/>
+          <h4>{name}</h4>
+          <p className='title'>{title}</p>
+          <p className='text'>{quote}</p>
+          <FaQuoteRight className='icon' />
          </article>
+         );
         })}
+        <button className='prev'>
+          <FiChevronLeft />
+        </button>
+        <button className='next'>
+          <FiChevronRight />
+        </button>
       </div>
     </section>
     );
