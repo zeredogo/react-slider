8adc126ce96d75100c3a878b471a15784372f960
zeredogo <zeredogo@gmail.com>
2022-07-27 15:04:59 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -8,20 +8,41 @@ const App = () => {
   const [people, setPeople] = useState(data);
   const [index, setIndex] =  useState(0);
 
+  useEffect(()=> {
+    const lastIndex = people.length -1;
+    if(index < 0){
+      setIndex(lastIndex);
+    }
+    if(index > lastIndex){
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(()=>{
+   let slider = setInterval(()=>{
+      setIndex(index + 1)
+    }, 5000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
     <section className='section'>
       <div className='title'>
         <h2>
-          <span>/</span>reviews
+          <span>/</span>reviews 
         </h2>
       </div>
       <div className='section-center'>
         {people.map((person, personIndex) =>{
          const {id, image, name, title, quote} = person;
          // more stuff comming up
-         let position = 'nextSlider';
+         let position = 'nextSlide';
          if (personIndex === index) {
           position = 'activeSlide';
+         }
+         if(personIndex === index - 1 || 
+          (index === 0 && personIndex === people.length - 1)
+          ) {
+          position = 'lastSlide';
          }
          
          return (<article className={position} key={id}>
@@ -33,13 +54,14 @@ const App = () => {
          </article>
          );
         })}
-        <button className='prev'>
+        <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className='next'>
+        <button className='next' onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
+      <p className="footer"><small>Krypto Zone &copy; 2022</small></p>
     </section>
     );
 }
