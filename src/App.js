0988738130a0b104627e7a6a0b7a1f56ec3bdc39
zeredogo <zeredogo@gmail.com>
2022-07-27 13:09:42 +0100

    updating commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,18 @@ import data from './data';
 import './App.css';
 
 const App = () => {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] =  useState(0);
+
   return (
-  <h2>slider project setup</h2>
-  );
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+    </section>
+    );
 }
 
 export default App
