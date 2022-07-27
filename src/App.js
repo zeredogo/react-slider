a8e2f187ab6cadd9e17ad365e5d8868a741a1bd8
zeredogo <zeredogo@gmail.com>
2022-07-27 12:56:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+import './App.css';
+
+const App = () => {
+  return (
+  <h2>slider project setup</h2>
+  );
+}
+
+export default App
